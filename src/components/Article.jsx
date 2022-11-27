e0af8134b34fdf8c41bc6238e14f15977616b44f
Phuong My <101578258+phuongmy1602@users.noreply.github.com>
2022-11-27 20:41:05 +0700

    css image gallery

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,32 +1,26 @@
 import { format } from "date-fns";
 
-function Article({ id, urls, user, created_at}) {
+function Article({ id, urls, user, created_at }) {
   return (
-      <div>
-        <article key={id}>
-          <img src={urls.regular} alt={user.username} />
+    <div className="p-5 rounded-3xl shadow-md bg-white">
+      <article key={id} className="rounded-3xl">
+        <img
+          src={urls.regular}
+          alt={user.username}
+          className="h-52 object-fit object-cover w-full lg:h-80 rounded-3xl"
+        />
 
-          <div>
-            <article>
-              <img src={user.profile_image.medium} alt={user.username} />
-              <ul>
-                <li>{user.name}</li>
-                <li>{format(new Date(created_at), "dd MMMM yyyy")}</li>
-              </ul>
-            </article>
-
-            <article>
-              <a
-                href={`https://instagram.com/${user.instagram_username}`}
-                target="_blank"
-                rel="noreferrer"
-              >
-                {user.instagram_username}
-              </a>
-            </article>
-          </div>
-        </article>
-      </div>
+        <div className="p-5 pb-0 flex flex-col md:flex-row items-start md:items-center justify-between">
+          <article className="flex items-center justify-start">
+            <img src={user.profile_image.medium} alt={user.username} className="rounded-full mr-2 w-10 md:w-auto" />
+            <ul>
+              <li className="text-slate-800 font-bold">{user.name}</li>
+              <li className="text-sm text-slate-800 opacity-75">{format(new Date(created_at), "dd MMMM yyyy")}</li>
+            </ul>
+          </article>
+        </div>
+      </article>
+    </div>
   );
 }
 export default Article;
--- src/components/GetImages.jsx
+++ src/components/GetImages.jsx
@@ -1,35 +1,33 @@
-import { useState, useEffect } from "react"
-import Article from "./Article"
+import { useState, useEffect } from "react";
+import Article from "./Article";
 
-export default function GetImages() {
-  const [images, setImages] = useState([])
+function GetImages() {
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
     const fetchImages = async () => {
       const response = await fetch(
         `https://api.unsplash.com/photos?client_id=dGQW3GmLHf7erXuRLChVXH1C-MWMgYYIHlugEwcSMt4`
-      )
-      const data = await response.json()
-      console.log(data)
-      setImages(data)
-    }
+      );
+      const data = await response.json();
+      console.log(data);
+      setImages(data);
+    };
 
-    fetchImages()
-  }, [])
+    fetchImages();
+  }, []);
 
   return (
     <>
-      <div>
-        <h2>
-          Recommended For You
-        </h2>
+      <div className="container mx-auto px-5 2xl:px-0">
+        <p className="text-slate-800 font-bold text-3xl md:text-4xl lg:text-6xl my-10 lg:mt-20 lg:mb-14">Recommended For You</p>
 
         {!images ? (
           <div>
             <h1>Loading...</h1>
           </div>
         ) : (
-          <section >
+          <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3 pb-20 lg:container">
             {images.map((image) => (
               <Article key={image.id} {...image} />
             ))}
@@ -37,5 +35,6 @@ export default function GetImages() {
         )}
       </div>
     </>
-  )
+  );
 }
+export default GetImages;
