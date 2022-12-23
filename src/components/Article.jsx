36fefe18374870db00859702f44364f0c459b3b7
Phuong My <101578258+phuongmy1602@users.noreply.github.com>
2022-12-23 23:12:49 +0700

    refactor css Images

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,25 +1,29 @@
 import { format } from "date-fns";
 
-function Article({ id, urls, user, created_at }) {
+function Article({ id, urls, user, created_at, alt_description }) {
   return (
-    <div className="p-5 rounded-3xl shadow-md bg-white">
-      <article key={id} className="rounded-3xl">
+    <div>
+      <div key={id}>
         <img
           src={urls.regular}
-          alt={user.username}
-          className="h-52 object-fit object-cover w-full lg:h-80 rounded-3xl"
+          title={alt_description}
+          alt={alt_description}
+          className="h-52 object-fit object-cover w-full lg:h-80"
         />
-
-        <div className="p-5 pb-0 flex flex-col md:flex-row items-start md:items-center justify-between">
-          <article className="flex items-center justify-start">
-            <img src={user.profile_image.medium} alt={user.username} className="rounded-full mr-2 w-10 md:w-auto" />
-            <ul>
-              <li className="text-slate-800 font-bold">{user.name}</li>
-              <li className="text-sm text-slate-800 opacity-75">{format(new Date(created_at), "dd MMMM yyyy")}</li>
-            </ul>
-          </article>
+        <div className="flex items-center my-1">
+          <img
+            src={user.profile_image.small}
+            alt={user.username}
+            className="rounded-full"
+          />
+          <ul>
+            <li className="text-sm">{user.name}</li>
+            <li className="text-xs opacity-75">
+              {format(new Date(created_at), "MMMM yyyy")}
+            </li>
+          </ul>
         </div>
-      </article>
+      </div>
     </div>
   );
 }
