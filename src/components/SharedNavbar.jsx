f1b067dd8c2b2ff565495254ec2d72d9abc48884
Phuong My <101578258+phuongmy1602@users.noreply.github.com>
2022-12-26 21:42:58 +0700

    refactor UI Header

--- src/components/SharedNavbar.jsx
+++ src/components/SharedNavbar.jsx
@@ -9,10 +9,10 @@ import {
 
 function SharedNavbar() {
   return (
-    <header>
+    <header className="sharednav">
       <nav className="navbar">
         <div>
-          <NavLink to="/" className={({ isActive }) => (isActive ? 'link activepages' : 'pages')}>
+          <NavLink to="/">
             <img
               src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAJ1BMVEUAAAD////V1dWWlpZ0dHTPz89qamrh4eGQkJDk5OTR0dGtra3Y2NgWv+ptAAABL0lEQVR4nO3cMQ7CQAxFwQCBQOD+56Wms2RHxjDvAKs/vbXLqaR1qW+tmbbUPENISJiIMBghIWEiwmCEhISJCIMREhImIgxGSEiYiDAYISFhIsJghISEiQiDERISJiIMRkhImIgwGCEhYSLCYISEhIkIgxESEiYiDPb7wtsBwlvNtCLhdq5vq5lWJPziCOdHOD/C+RHOj3B+hPMjnB/h/AjnRzg/wvkRzo9wfoTzI5wf4fz+QHip6P44YNrjXrKt6KrgeoDwWrStJkJCwv4ICQn7IyQk7I+QkLA/QkLC/ggJCfsjJCTsj5CQsD9CQsL+CAkJ+yMkJOyPkJCwP0JCwv4ICQn7IyQk7G8/QLh3oz56vsq/oH09u1GSJEmSJEmSJEmSJEmSJEmSJEmSJP1rbwwaXQZE0kwUAAAAAElFTkSuQmCC"
               alt="logo image"
